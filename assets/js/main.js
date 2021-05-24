64e8f22839eabc53287d68e80f37c717cb68e969
greattambino <11471899+greattambino@users.noreply.github.com>
2021-05-24 16:52:44 -0700

    Setup the Three.js WebGL renderer and create a scene

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+/* global THREE */
+
 'use strict';
 
 (function() {
@@ -11,7 +13,34 @@
      * @constructor
      */
     function IconGalaxy() {
+      // ------------------------------
+      // Setup
+      // ------------------------------
+      this.setupScene();
     }
+
+    /**
+     * Initial setup that creates the scene and renderer.
+     */
+    IconGalaxy.prototype.setupScene = function() {
+      // Screen dimensions.
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+
+      // Create the scene.
+      this.scene = new THREE.Scene();
+
+      // Create the renderer.
+      var performAntialias = window.devicePixelRatio == 1.0;
+      this.renderer = new THREE.WebGLRenderer({antialias: performAntialias});
+      this.renderer.setClearColor(0x0);
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+      this.renderer.setSize(this.width, this.height);
+
+      // Add the renderer to the dom.
+      var container = document.getElementById('container');
+      container.appendChild(this.renderer.domElement);
+    };
 
     return IconGalaxy;
   })();
