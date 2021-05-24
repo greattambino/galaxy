bd53163ca19243665b4b31bbc411b588fbe4ee4e
greattambino <11471899+greattambino@users.noreply.github.com>
2021-05-24 16:37:35 -0700

    Create an IconGalaxy class that instantiates upon window load

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function() {
+  /**
+   * A class that displays icon particles joined together to form a word
+   *     in space.
+   * @author Marc Tambara
+   */
+  var IconGalaxy = (function() {
+    /**
+     * @constructor
+     */
+    function IconGalaxy() {
+    }
+
+    return IconGalaxy;
+  })();
+
+  window.addEventListener('load', function() {
+    new IconGalaxy();
+  });
+})();
